d30f5664206ff28cfdaf1695fda6a866e853d6cf
Nadav <161940458+NadavOsh@users.noreply.github.com>
2025-08-12 15:45:57 +0300

    Update menu.c

--- Src/menu.c
+++ Src/menu.c
@@ -1,7 +1,3 @@
-/*
-תובל לנגנס - 205801095
-נדב אושרוב - 318799806
-*/
 #define _CRT_SECURE_NO_WARNINGS
 #include "menu.h"
 #include <stdio.h>
@@ -301,6 +297,7 @@ void showMenu(unsigned int employeeLevel) {
     }
 
     printf("--------------------\n");
+
 
 
 }
